488e88535ed29a157b128e071e3adbd84099ce29
paalse <paalse@list.ru>
2019-09-19 21:15:11 +0400

    Lesson_2

--- src/Lesson_2/MyArrayDataException.java
+++ src/Lesson_2/MyArrayDataException.java
@@ -1,5 +1,17 @@
 package Lesson_2;
 
+/**
+ * Исключение при некорректных данных в массиве
+ */
 public class MyArrayDataException extends Exception {
+    private int number;
 
+    public MyArrayDataException(String string) {
+        super("В массиве содержатся не корректные данные: " + string);
+        this.number = 101;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
