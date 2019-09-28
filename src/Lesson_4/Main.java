eaf7e1f11070e820ede39b24af9fbab0a7357aed
paalse <paalse@list.ru>
2019-09-28 22:13:11 +0400

    Lesson_4

--- src/Lesson_4/Main.java
+++ src/Lesson_4/Main.java
@@ -6,17 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class MainChat extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("chat.fxml"));
-        primaryStage.setTitle("My chat");
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("My Chat");
         primaryStage.setScene(new Scene(root, 350, 375));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
