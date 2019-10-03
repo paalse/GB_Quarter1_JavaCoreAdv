24e9edb756e3498dadc8190ee61fd419ec218910
paalse <paalse@list.ru>
2019-10-03 11:51:54 +0400

    Lesson_5 оптимизация кода

--- src/Lesson_5/Main.java
+++ src/Lesson_5/Main.java
@@ -34,85 +34,53 @@ static final int h = size / 2;
 */
 package Lesson_5;
 
+import java.util.Arrays;
+
 public class Main {
 
-    static final int size = 10000000;
-    static final int h = size / 2;
-
-    /**
-     * Метод обработки массива без потоков
-     */
-    public static void methodNoThread() {
-        float[] arr = new float[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = 1;
-        }
-        long a = System.currentTimeMillis();
-
-        for (int i = 0; i < size; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        System.out.println("No thread method: " + (System.currentTimeMillis() - a));
-    }
-
-    /**
-     * Метод обработки массива с помощь потоков
-     */
-    public static void methodThread() {
-        float[] arr = new float[size];
-        float[] a1 = new float[h];
-        float[] a2 = new float[h];
-        for (int i = 0; i < size; i++) {
-            arr[i] = 1;
-        }
-        long a = System.currentTimeMillis();
-
-        //Разбиение массива
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
-
-        // Поток1
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < h; i++) {
-                    a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
-        });
-
-        // Поток2
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < h; i++) {
-                    a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
-        });
-
-        // Запуск потоков
-        t1.start();
-        t2.start();
-
-        // Ожидание выполнения обоих потоков
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        //Склейка массива
-        System.arraycopy(a1, 0, arr, 0, h);
-        System.arraycopy(a2, 0, arr, h, h);
-
-        System.out.println("Thread method: " + (System.currentTimeMillis() - a));
-    }
-
-
     public static void main(String[] args) {
-        methodNoThread();
-        methodThread();
+        final int SIZE = 10000000;                  // Размер всего массива
+        final int THREADS_COUNT = 10;           // Количество потоков 1, либо четное количество
+        final int PART_SIZE = SIZE / THREADS_COUNT; // Размер части массива
+
+        float[] arr_origin = new float[SIZE];
+        float[][] arr_part = new float[THREADS_COUNT][PART_SIZE];
+        Arrays.fill(arr_origin, 1f);            // Заполнение массива 1-ми
+
+        Thread[] threads = new Thread[THREADS_COUNT];            // Массив с потоками
+
+        long a = System.currentTimeMillis();
+
+        // Разбиение исходного массива  и создание потоков
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            System.arraycopy(arr_origin, i * PART_SIZE, arr_part[i], 0, PART_SIZE);
+
+            final int u = i; // i не простаскивается во вложенный класс поэтому создали доп переменную u, с final не понятно, работает с ним и без него
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // считаем массив со сдвигом
+                    int n = u * PART_SIZE;
+                    for (int j = 0; j < PART_SIZE; j++, n++) {
+                        arr_part[u][j] *= (Math.sin(0.2f + n / 5) * Math.cos(0.2f + n / 5) * Math.cos(0.4f + n / 2));
+                    }
+                }
+            });
+            threads[i].start(); // Запуск потока
+        }
+
+        // Ждем завершения всех потоков
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // Объединение частей массивов в исходный
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            System.arraycopy(arr_part[i], 0, arr_origin, i * PART_SIZE, PART_SIZE);
+        }
+        System.out.println("Working time: " + (System.currentTimeMillis() - a));
     }
 }
