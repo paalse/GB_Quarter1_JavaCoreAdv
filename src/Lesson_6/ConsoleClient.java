0f53b77c8d48284b37d1f1c876df649c14f68e57
paalse <paalse@list.ru>
2019-10-04 11:56:01 +0400

    Lesson_6

--- src/Lesson_6/ConsoleClient.java
+++ src/Lesson_6/ConsoleClient.java
@@ -1,4 +1,56 @@
 package Lesson_6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ConsoleClient {
+    static final String ADDRESS = "localhost";
+    static final int PORT = 8186;
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket socket = new Socket(ADDRESS, PORT);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner consoleIn = new Scanner(System.in);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String str = in.readUTF();
+                            System.out.println(str);
+                        } catch (IOException e) {
+                            //e.printStackTrace();
+                            System.out.println("Сервер не доступен!");
+                            break;
+                        }
+                    }
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String str = consoleIn.nextLine();
+                            out.writeUTF(str);
+                        } catch (IOException e) {
+                            //e.printStackTrace();
+                            System.out.println("Сервер не доступен!");
+                            break;
+                        }
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Lesson_6/ConsoleServer.java
+++ src/Lesson_6/ConsoleServer.java
@@ -1,4 +1,62 @@
 package Lesson_6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ConsoleServer {
+    static final String ADDRESS = "localhost";
+    static final int PORT = 8186;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+            while(true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент подключился!");
+
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                Scanner consoleIn = new Scanner(System.in);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            try {
+                                String str = in.readUTF();
+                                System.out.println(str);
+                            } catch (IOException e) {
+                                //e.printStackTrace();
+                                System.out.println("Клиент отключился!");
+                                break;
+                            }
+                        }
+                    }
+                }).start();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            try {
+                                String str = consoleIn.nextLine();
+                                out.writeUTF(str);
+                            } catch (IOException e) {
+                                //e.printStackTrace();
+                                System.out.println("Клиент отключился!");
+                                break;
+                            }
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
